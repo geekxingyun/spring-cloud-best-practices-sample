ee3625cdbdce3e41b2c8f99dc32f8d41c78003dc
qing-feng.zhao <fairy_xingyun@hotmail.com>
2020-01-31 15:00:56 +0800

    init project

---
+++ single-architecture-spring-boot-shopping-sample/src/test/java/com/xingyun/singlearchitecturespringbootshoppingsample/SingleArchitectureSpringBootShoppingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingyun.singlearchitecturespringbootshoppingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleArchitectureSpringBootShoppingSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
