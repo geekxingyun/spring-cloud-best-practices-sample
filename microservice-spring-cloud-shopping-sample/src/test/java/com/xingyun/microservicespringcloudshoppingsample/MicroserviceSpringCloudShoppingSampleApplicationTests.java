533f35542a012bf79b34f915cf6c7ef7adc57201
qing-feng.zhao <fairy_xingyun@hotmail.com>
2020-03-21 13:47:19 +0800

    init project

---
+++ microservice-spring-cloud-shopping-sample/src/test/java/com/xingyun/microservicespringcloudshoppingsample/MicroserviceSpringCloudShoppingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingyun.microservicespringcloudshoppingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringCloudShoppingSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
